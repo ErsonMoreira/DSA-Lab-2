c320301209c855bf45b1df742d10c04cea5a9b9f
ErsonMoreira <2013110592@ub.edu.bz>
2025-10-07 12:29:41 -0600

    Add files via upload

---
+++ bst1.cpp
@@ -0,0 +1,11 @@
+struct BSTNode {
+    int data;
+    BSTNode* left;
+    BSTNode* right;
+
+    BSTNode(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
